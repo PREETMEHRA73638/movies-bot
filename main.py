d8b4499f5c2a5504ab82b5503bf859a7f61bfc2c
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-11-09 16:41:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
     InputTextMessageContent
-from TeamTeleRoid.forcesub import ForceSub
+from  import fsub.py
 import asyncio
 
 # Bot Client for Inline Search
